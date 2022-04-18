5391c4ab4cfad90e9b69e9832e984e00d3198dc5
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-04-18 11:56:27 +0200

    ej 4

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
     </title>
     <h1>Mi primera página PHP</h1>
     <p>Página de pruebas PHP</p>
+    <?php
+    $variable1=0;
+    $variable2=0;
+    $suma=$variable1+$variable2;
+    echo "$variable1 + $variable2 = $suma";
+    ?>
 </body>
 </html>
