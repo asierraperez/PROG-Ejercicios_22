dc75d80f3d56b5791543f45ef45775c461b829f2
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-04-18 12:01:58 +0200

    ej 5

--- index.php
+++ index.php
@@ -9,7 +9,14 @@
     $variable1=0;
     $variable2=0;
     $suma=$variable1+$variable2;
-    echo "<p>$variable1 + $variable2 = $suma</p>";
+    $p1=3;
+    $p2=$p1*3;
+    $p3=($p1+$p2)/4;
+    echo "<p>$variable1 + $variable2 = $suma<br>
+    p1 = $p1<br>
+    p2 = $p1 * 3 = $p2<br>
+    p3 = ($p1 + $p2) / 4 = $p3<br>
+    </p>";
     ?>
 </body>
 </html>
