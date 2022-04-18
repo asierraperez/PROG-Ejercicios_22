42cb83a5c529db6ef502cd9aff77bab36264eb0d
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-04-18 12:10:07 +0200

    ej 6

--- index.php
+++ index.php
@@ -6,16 +6,24 @@
     <h1>Mi primera página PHP</h1>
     <p>Página de pruebas PHP</p>
     <?php
-    $variable1=0;
-    $variable2=0;
+    $variable1=1;
+    $variable2=2;
     $suma=$variable1+$variable2;
-    $p1=3;
+    $p1=1;
     $p2=$p1*3;
-    $p3=($p1+$p2)/4;
+    
+    if ($p2 > 5) {
+        $div=4;
+    } else{
+        $div=8;        
+    }
+
+    $p3=($p1+$p2)/$div;
+
     echo "<p>$variable1 + $variable2 = $suma<br>
     p1 = $p1<br>
     p2 = $p1 * 3 = $p2<br>
-    p3 = ($p1 + $p2) / 4 = $p3<br>
+    p3 = ($p1 + $p2) / $div = $p3<br>
     </p>";
     ?>
 </body>
