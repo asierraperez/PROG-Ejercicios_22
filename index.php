e79a5be8fd5bb9e77448142b5b471c8d0d3d6006
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-04-18 12:27:18 +0200

    ej 8

--- index.php
+++ index.php
@@ -9,21 +9,54 @@
     $variable1=1;
     $variable2=2;
     $suma=$variable1+$variable2;
-    $p1=1;
-    $p2=$p1*3;
+    $p1=3;
+    $p2=$p1*3-1;
     
     if ($p2 > 5) {
-        $div=4;
-    } else{
-        $div=8;        
+        $p3=($p1+$p2)/4;
+        $Resultado_p2= "p2 = $p1 * 3 - 1 = $p2";
+        $Resultado_p3= "p3 = ($p1 + $p2) / 4 = $p3<br>";        
+    } elseif ($p2==5) {
+        $p3=$p1+$p2;
+        $Resultado_p2= "p2 = $p2";
+        $Resultado_p3= "p3 = $p1 + $p2 = $p3<br>";
+    }else{
+        $p3=($p1+$p2)/8;
+        $Resultado_p2= "p2 = $p1 * 3 - 1 = $p2";
+        $Resultado_p3= "p3 = ($p1 + $p2) / 8 = $p3<br>";
+    }
+    
+    echo "<p>IF...ELSEIF...ELSE<br>
+    $variable1 + $variable2 = $suma<br>
+    p1 = $p1<br>
+    $Resultado_p2<br>
+    $Resultado_p3<br>
+    </p>";
+
+    switch ($p2) {
+        case ($p2 > 5):
+            $p3=($p1+$p2)/4;
+            $Resultado_p2= "p2 = $p1 * 3 - 1 = $p2";
+            $Resultado_p3= "p3 = ($p1 + $p2) / 4 = $p3<br>"; 
+            break;
+        case 5:
+            $p3=$p1+$p2;
+            $Resultado_p2= "p2 = $p2";
+            $Resultado_p3= "p3 = $p1 + $p2 = $p3<br>";
+            break;      
+        default:
+            $p3=($p1+$p2)/8;
+            $Resultado_p2= "p2 = $p1 * 3 - 1 = $p2";
+            $Resultado_p3= "p3 = ($p1 + $p2) / 8 = $p3<br>";
+            break;
     }
 
-    $p3=($p1+$p2)/$div;
 
-    echo "<p>$variable1 + $variable2 = $suma<br>
+    echo "<p>SWITCH...CASE<br>
+    $variable1 + $variable2 = $suma<br>
     p1 = $p1<br>
-    p2 = $p1 * 3 = $p2<br>
-    p3 = ($p1 + $p2) / $div = $p3<br>
+    $Resultado_p2<br>
+    $Resultado_p3<br>
     </p>";
     ?>
 </body>
